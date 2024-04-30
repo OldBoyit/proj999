156dd79fe6fc713d5a9f21b9241043601c7f27f0
OldBoyit <129920436+OldBoyit@users.noreply.github.com>
2024-04-30 12:02:40 +0200

    Correzioni Database

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -11,7 +11,7 @@ function AdminLogin() {
 
     const handleAdminLogin = async () => {
         try {
-			const response = await fetch('https://chain.project999.it/.netlify/functions/login', {
+			const response = await fetch('.netlify/functions/login', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
