c6c51c3e634146fd1d55288a6086553ca73f4bcc
OldBoyit <129920436+OldBoyit@users.noreply.github.com>
2024-04-30 18:45:34 +0200

    Rifiniture

--- src/components/RemoveDigitalSignaturesPage.js
+++ src/components/RemoveDigitalSignaturesPage.js
@@ -64,7 +64,18 @@ function RemoveDigitalSignaturesPage() {
                 <button onClick={() => navigate('/producer-dashboard')} className={styles.dashboardButton}>Dashboard</button>
             </div>
             <div className={styles.exampleText}>
-                <p>Testo di esempio aggiunto qui.</p>
+                <center><p>. Questa pagina permette agli amministratori di rimuovere firme digitali esistenti, un processo importante per mantenere l'integrità e la sicurezza del sistema. La rimozione periodica o la sostituzione di firme digitali è una pratica raccomandata per prevenire abusi e garantire che solo gli utenti attuali e autorizzati possano registrare prodotti.</p>
+				<p>Caratteristiche principali della pagina </p>
+				<p>Visualizzazione e Selezione delle Firme:</p>
+				<p>Le firme digitali vengono caricate e visualizzate tramite una chiamata API, permettendo agli amministratori di visualizzarle facilmente.</p>
+				<p>Ogni firma può essere selezionata tramite una casella di controllo associata, facilitando la selezione multipla per operazioni di rimozione batch.</p>
+				<p>Gestione della Rimozione:</p>
+				<p>Un pulsante dedicato permette di iniziare il processo di rimozione delle firme selezionate. Attualmente, questa funzionalità è disabilitata per precauzione, mostrando un messaggio di alert quando tentata, per prevenire rimozioni accidentali o non autorizzate.</p>
+				<p>Sicurezza e Controllo di Accesso:</p>
+				<p>La possibilità di rimuovere firme digitali è una funzione potente che può influenzare l'accesso e i privilegi degli utenti. Per questo, tipicamente, l'accesso a questa pagina è strettamente controllato e limitato agli amministratori o agli utenti con permessi elevati.</p>
+				<p>Implicazioni della Rimozione di Firme Digitali:</p>
+				<p>Rimuovendo una firma digitale, il creatore associato a quella firma non potrà più registrare nuovi prodotti fino a quando non viene creata e registrata una nuova firma. Questo meccanismo serve per rafforzare la sicurezza, assicurando che le credenziali siano sempre aggiornate e valide.</p>
+				<p><b>La pratica di cambiare periodicamente le firme digitali è consigliata per ridurre il rischio di compromissione delle credenziali e per mantenere un alto livello di sicurezza all'interno dell'organizzazione.</b></p></center>
             </div>
         </div>
     );
--- src/components/UploadDigitalSignaturesPage.js
+++ src/components/UploadDigitalSignaturesPage.js
@@ -36,6 +36,16 @@ function UploadDigitalSignaturesPage() {
         <button type="submit">Carica Firma</button>
       </form>
       <button onClick={() => navigate('/producer-dashboard')}>Dashboard</button>
+	        <div className={styles.exampleText}>
+        <center><p>Caratteristiche principali della pagina</p>
+<p>Gestione delle Firme Digitali:</p>	
+<p>La pagina permette agli utenti di inserire e caricare nuove firme digitali.</p>	
+<p>Ogni firma è associata a una data di creazione, che viene automaticamente generata e mostrata all'utente per aumentare la trasparenza e fornire un timestamp di riferimento.</p>	
+<p>Conferma e Reset:</p>	
+<p>Dopo il caricamento della firma, un messaggio popup conferma il successo dell'operazione.</p>	
+<p>I campi del form vengono resettati dopo ogni invio, preparando l'interfaccia per un nuovo inserimento e mantenendo l'efficienza dell'utente.</p>	
+<p>Questa pagina non solo facilita l'aggiunta di nuove firme digitali ma rafforza anche la sicurezza e l'integrità del sistema di registrazione dei prodotti, garantendo che ogni prodotto sia tracciabile fino al suo creatore originale. Questo è particolarmente importante in ambienti regolamentati o in industrie dove l'autenticità e la provenienza del prodotto sono di massima importanza.</p>	</center>
+      </div>
     </div>
   );
 }
