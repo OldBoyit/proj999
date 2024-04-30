3d5ac63bc1a48c50aa2ee5bb3da7f52c2558b81c
OldBoyit <129920436+OldBoyit@users.noreply.github.com>
2024-04-30 17:39:41 +0200

    Rifiniture

--- src/components/ViewAssignedKeysPage.js
+++ src/components/ViewAssignedKeysPage.js
@@ -40,36 +40,38 @@ function ViewAssignedKeys() {
 
     return (
         <div className="assigned-keys-container">
+            <button onClick={() => navigate('/producer-dashboard')} className="dashboard-button">Dashboard</button>
             <h1>Chiavi Assegnate</h1>
             {keys.length > 0 ? (
                 <ul className="key-list">
-                {currentKeys.map((key) => (
-                    <li key={key._id} className="key-item">
-                        <p>Chiave Pubblica: {key.publicKey}</p>
-                        <p>Chiave Privata: {key.privateKey}</p>
-                        <p>Stato: {key.status}</p>
-                        <p>Attiva: {key.isActive ? 'Sì' : 'No'}</p>
-                    </li>
-                ))}
+                    {currentKeys.map((key) => (
+                        <li key={key._id} className="key-item">
+                            <p>Chiave Pubblica: {key.publicKey}</p>
+                            <p>Chiave Privata: {key.privateKey}</p>
+                            <p>Stato: {key.status}</p>
+                            <p>Attiva: {key.isActive ? 'Sì' : 'No'}</p>
+                        </li>
+                    ))}
                 </ul>
             ) : (
                 <p>Nessuna chiave assegnata.</p>
             )}
-			<div className="navigation-buttons">
-				{currentPage > 1 && (
-					<button onClick={prevPage} className="page-button">Pagina Precedente</button>
-				)}
-				{currentPage * keysPerPage < keys.length && (
-					<button onClick={nextPage} className="page-button">Pagina Successiva</button>
-				)}
-			</div>
-			<div className="page-description">
-				<p>
-					Questa pagina mostra le chiavi NFC assegnate ai prodotti. Ogni chiave elencata qui è già stata utilizzata per autenticare un prodotto specifico nel nostro sistema di tracciabilità. Le chiavi "Attiva" indicano che i prodotti associati non sono stati segnalati come contraffatti e continuano a essere validi e sicuri per l'uso nel mercato. La visibilità di chiavi pubbliche e private in questa pagina aiuta a mantenere la trasparenza e permette una gestione efficace del processo di autenticazione. È essenziale per assicurare che solo prodotti autentici siano circolanti, rafforzando così la fiducia dei clienti.
-				</p>
-			</div>
-		</div>
-	);
+            <div className="navigation-buttons">
+                {currentPage > 1 && (
+                    <button onClick={prevPage} className="page-button">Pagina Precedente</button>
+                )}
+                {currentPage * keysPerPage < keys.length && (
+                    <button onClick={nextPage} className="page-button">Pagina Successiva</button>
+                )}
+            </div>
+            <div className="page-description">
+                <p>
+                    Questa pagina mostra le chiavi NFC assegnate ai prodotti. Ogni chiave elencata qui è già stata utilizzata per autenticare un prodotto specifico nel nostro sistema di tracciabilità. Le chiavi "Attiva" indicano che i prodotti associati non sono stati segnalati come contraffatti e continuano a essere validi e sicuri per l'uso nel mercato. La visibilità di chiavi pubbliche e private in questa pagina aiuta a mantenere la trasparenza e permette una gestione efficace del processo di autenticazione. È essenziale per assicurare che solo prodotti autentici siano circolanti, rafforzando così la fiducia dei clienti.
+                </p>
+            </div>
+            <div style={{ marginBottom: '20px' }}></div>
+        </div>
+    );
 }
 
 export default ViewAssignedKeys;
